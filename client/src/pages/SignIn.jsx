5daab17d1d0415b9b5bc2cf94af28f71f92f62d6
Rem Simiyu <remsimiyu@gmail.com>
2025-09-29 19:33:51 +0300

    Complete sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const SignIn = () => {
+const SignIn = ({setUser}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate  = useNavigate();
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setLoading(true);
+  setError(null);
+
+  try{
+    const res = await axios.post('/api/auth/signin', {
+      email,
+      password
+    });
+
+    if(res.data.success) {
+      setError(null);
+      setUser(res.data.user);
+      navigate('/');
+    } else {
+      setError(res.data.message || "unexpected error");
+    }
+  } catch(err) {
+    setError(err.response?.data?.message);
+  } finally {
+    setLoading(false)
+  }
+}
+
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
+      {error && 
+        <p className='text-red-500 mb-4 text-center'>
+          {error}
+        </p>
+      }
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+        <input 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            placeholder='Email'
+            className='border p-3 rounded-lg focus:outline-none'
+            required
+        />
+        <input 
+          type="password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Password'
+          className='border p-3 rounded-lg focus:outline-none'
+          required
+        />
+        <button 
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 cursor-pointer'>
+            { loading ? "Sign in..." : "Sign in" }
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Don't have an account?
+          <Link 
+            to="/signup" 
+            className='text-blue-700'>
+            sign up
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
