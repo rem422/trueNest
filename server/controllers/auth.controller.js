28ae3a7fd50365b2a1d48ac97ff827c4d3aefc4b
Rem Simiyu <remsimiyu@gmail.com>
2025-10-01 04:05:29 +0300

    Fixing bugs

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,39 +1,17 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 import { errorHandler2 } from '../middlewares/errorHandler.js';
 import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
+    const hashPassword = await bcrypt.hashSync(password, 12);
+    const newUser = await User({ username, email, password: hashPassword });
 
     try{
-        if(!username || !email || !password) {
-            return res.status(400).json({message: 'Please fill all the fields'});
-        }
-
-        const userExists = await User.findOne({email});
-        if(userExists) {
-            return res.status(400).json({message: 'User already exists'})
-        }
-
-        const newUser = await User({
-            username, 
-            email, 
-            password
-        });
-
         await newUser.save();
-        
-        res.status(201).json({
-            success: true,
-            message: 'User created successfully!',
-            user: {
-                id: newUser._id,
-                username: newUser.username,
-                email: newUser.email
-            }
-        });
-    
-    } catch(err){
+        res.status(201).json('User created successfully!');
+    } catch(err) {
         next(err);
     }
 }
