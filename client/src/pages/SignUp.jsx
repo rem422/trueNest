fc38031f0ed403454cb97adbe652fc8f03df7167
Rem Simiyu <remsimiyu@gmail.com>
2025-10-13 02:56:30 +0300

    Fixing Bugs

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,74 +1,65 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const SignUp = ({ setUser }) => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const navigate  = useNavigate();
+const SignUp = () => {
+const [formData, setFormData] = useState({});
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false);
 
+const handleChange = (e) => {
+  setFormData({
+    ...formData,
+    [e.target.id]: e.target.value,
+  });
+};
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-  setLoading(true);
-  setError(null);
-
-  try{
-    const res = await axios.post('/api/auth/signup', {
-      username, 
-      email, 
-      password
-    });
-
-    if(res.data.success) {
-      setError(null);
-      setUser(res.data.user);
-      navigate('/');
-    } else {
-      setError(res.data.message || "Unexpected error");
+  setLoading(true)
+  const res = await fetch('/api/auth/signup', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
     }
-
-  } catch(err) {
-    setError(err.response?.data?.message);
-  } finally {
-    setLoading(false);
-  }
-}
+  );
+  
+  const data  = await res.json();
+  console.log(data);
+  
+  setLoading(false);
+};
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      {error && 
-        <p className='text-red-500 mb-4 text-center'>
-          {error}
-        </p>
-      }
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+      <form 
+          onSubmit={handleSubmit} 
+          className='flex flex-col gap-4 '>
         <input 
-          type="username" 
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          id="username"
           placeholder='Username'
           className='border p-3 rounded-lg focus:outline-none'
+          onChange={handleChange}
           required
         />
         <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
+            type="email"
+            id="email"
             placeholder='Email'
             className='border p-3 rounded-lg focus:outline-none'
+            onChange={handleChange} 
             required
         />
         <input 
-          type="password" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          id="password"
           placeholder='Password'
           className='border p-3 rounded-lg focus:outline-none'
+          onChange={handleChange}
           required
         />
         <button 
